ad929c34bf0d9ffc8a8d3c5a66ce99b04ff7085a
MorenaFischione <fischione.morena@gmail.com>
2021-10-06 12:47:13 +0200

    inizzializzazione esercizio

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1 class="text-danger">Header</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+
+}
+</script>
+
+<style lang="scss">
+@import "../style/variables.scss";
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <h1 class="my_main">Main</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name : "Main",
+
+}
+</script>
+
+<style lang="scss">
+@import "../style/variables.scss";
+
+.my_main {
+    font-family: $main-font;
+}
+
+</style>
