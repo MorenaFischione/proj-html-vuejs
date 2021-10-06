3fab655eff721c7f9b7bdd261ee928c52fe9b0f8
MorenaFischione <fischione.morena@gmail.com>
2021-10-06 20:18:04 +0200

    Bozza jambotron

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-      <h1 class="my_main">Main</h1>
-  </div>
+    <section class="jambotron">
+        <div class="container-fluid my_container">
+            <div class="row">
+                <div class="col-10 p-3">
+                    <h3>Certified fitness professional</h3>
+                    <h1>Take control of your health</h1>
+                    <i class="fas fa-angle-up"></i><i class="fas fa-angle-up"></i><i class="fas fa-angle-up"></i>
+                </div>
+                <div class="col-2">
+
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -14,8 +25,20 @@ export default {
 <style lang="scss">
 @import "../style/variables.scss";
 
-.my_main {
-    font-family: $main-font;
+.jambotron {
+    background-image: url(../assets/images/slider82x-scaled.jpg);
+    height: 900px;
+
+    .my_container {
+        width: 80%;
+
+        .fas {
+            font-size: 1.2rem;
+        }
+    }
+
+    
+        
 }
 
 </style>
